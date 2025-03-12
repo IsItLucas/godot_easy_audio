9dc685960dead7dba3a742065ea8b472e4859c0a
IsItLucas? <lucas.galonet.com@gmail.com>
2025-03-11 21:23:15 -0300

    Addon full rework

--- addons/easy_audio/plugin.gd
+++ addons/easy_audio/plugin.gd
@@ -1,46 +1,26 @@
 @tool extends EditorPlugin
 
 
-# Added constants to make the creation of new "Godot Easy ..." plugins easier.
-const AUTOLOAD_NAME := "Audio"
-const FOLDER_NAME := "easy_audio"
-const SETTINGS_NAME := "audio"
+## This script is responsible for setting up the plugin and its autoload.
 
-# All custom project settings created by the addon should be in here for an easy clean up and addition.
-const CUSTOM_SETTINGS: Array[Array] = [
-	["debug_log", true],
-	["audio/max_clipping", 64],
-	["audio/default_sfx_bus", "SFX"],
-	["audio/default_music_bus", "Music"],
-]
+
+## The name of the autoload that is going to be added by this plugin.
+const AUTOLOAD_NAMES := ["Audio", "Music"]
+
+## Where the autoload script / scene is located.
+const FOLDER_NAME := "easy_audio"
+
+## The autoload file name and extension.
+const FILE_NAMES := ["audio.gd", "music.gd"]
 
 
 func _enter_tree() -> void:
-	# Add GEA autoload.
-	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/" + FOLDER_NAME + "/autoload.gd")
-	
-	# Add GES settings with compatibility for other "Godot Easy ..." plugins.
-	for pair: Array in CUSTOM_SETTINGS:
-		var setting_name: String = pair[0]
-		var setting_value: Variant = pair[1]
-		set_setting(setting_name, setting_value)
+	# Add autoload.
+	for i: int in range(AUTOLOAD_NAMES.size()):
+		add_autoload_singleton(AUTOLOAD_NAMES[i], "res://addons/" + FOLDER_NAME + "/" + FILE_NAMES[i])
 
 
 func _exit_tree() -> void:
-	# Remove GEA autoload.
-	remove_autoload_singleton(AUTOLOAD_NAME)
-	
-	# Remove custom settings.
-	for pair: Array in CUSTOM_SETTINGS:
-		var setting_name: String = pair[0]
-		var path := AudioHelper.get_setting_path(setting_name)
-		ProjectSettings.set_setting(path, null)
-
-
-func set_setting(setting_name: String, setting_value: Variant) -> void:
-	# Add / remove a project setting. What this line does is defined by setting_value.
-	# If the value is null, the setting will be removed. But if not, the setting is going to be added.
-	var path := AudioHelper.get_setting_path(setting_name)
-	if not ProjectSettings.has_setting(path):
-		ProjectSettings.set_setting(path, ProjectSettings.get_setting(path, setting_value))
-		ProjectSettings.set_initial_value(path, setting_value)
+	# Remove autoload.
+	for i: int in range(AUTOLOAD_NAMES.size()):
+		remove_autoload_singleton(AUTOLOAD_NAMES[i])
